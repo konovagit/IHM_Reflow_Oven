9ce8dcc7fcb9f960c1a5a5aa6c26ca60f1a850f6
konovagit <corentincorde@orange.fr>
2015-07-10 13:52:55 +0100

    delete library Qtplot because doesn't work with my version

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 #include <QFont>
 #include <QIcon>
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     MainWindow w;
     w.show();
+
+
+    //QwtPlot myPlot;
+    //myPlot.show();
 
     return a.exec();
 }
